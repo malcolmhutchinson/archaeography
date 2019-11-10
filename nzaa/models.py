25cf170ce6b0c280fff47da15dade5c1ab1cebe3
Malcolm Hutchinson <malcolm@archaeography.co.nz>
2019-11-10 20:31:21 +1300

    Small changes around normalisation.

--- nzaa/models.py
+++ nzaa/models.py
@@ -2465,10 +2465,10 @@ class Document(models.Model):
             bits = item.split()
             try:
                 item.ordinal = int(bits[-1])
-                #print "ordinal set to", int(bits[-1])
+
             except TypeError:
                 item.ordinal = 0
-                #print "ordinal set to 0"
+
 
 class DocFile(models.Model):
     """A DocFile is a file expression of a document.
